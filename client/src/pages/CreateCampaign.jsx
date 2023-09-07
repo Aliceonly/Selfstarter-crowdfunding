47154f79a391fe380cb661f3500ba8a97daec52b
Aliceonly <1139045289@qq.com>
2023-09-07 15:03:40 +0800

    framework&jsx

---
+++ client/src/pages/CreateCampaign.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { ethers } from 'ethers'
+
+import { money } from '../assets';
+import { CustomButton } from '../components';
+
+const CreateCampaign = () => {
+  return (
+    <div>CreateCampaign</div>
+  )
+}
+
+export default CreateCampaign
