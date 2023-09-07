6d7121a789f32294ce0a5aefa282d231efc77705
Aliceonly <1139045289@qq.com>
2023-09-07 18:31:33 +0800

    Display Component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useStateContext } from '../context';
+import { DisplayCampaigns } from '../components';
 
 
 const Home = () => {
@@ -19,7 +20,11 @@ const Home = () => {
   },[address,contract])
 
   return (
-    <div>Home</div>
+    <DisplayCampaigns
+      title="All Campaigns"
+      isLoading={isLoading}
+      campaigns={campaigns}
+    />
   )
 }
 
